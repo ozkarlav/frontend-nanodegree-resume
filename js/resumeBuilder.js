c6b39febafe26c40e7f145c64197f909e79064c3
Oscar Laverde <ozkarlav@yahoo.com>
2016-02-06 17:21:34 -0500

    Creating content info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,89 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"fullname" : "Oscar Laverde", 
+	"role" : "Web-Developer", 
+	"contacts" : {
+		"mobile" : "519-619-3907",
+		"email" : "info@oscarlab.ca", 
+		"github" : "ozkarlav",
+		"twitter" : "@oscarlav",
+		"location" : "London, ON, Canada"
+	},
+	"picture" : "images/oscar.jpg", 
+	"welcomemsg" : "Welcome to my Bio Page", 
+	"skills" : ["e-commerce", "HTML", "CSS", "Windows"]
+};
+
+var work = {
+	"jobs": [	{
+		"employer": "ABC Cork Co.",
+		"title": "Web Developer, Graphic Designer, IT/Project Manager.",
+		"location": "London, ON",
+		"dates": "2005 - 2016",
+		"description": "Responsible for design and daily maintenance of a global website and many of its localized sites. Supervise the development, security and performance of the online business working with a team within the organization as well as external 3rd party agencies, successfully increasing company revenues. Investigate various security incidents caused by malicious activities, and identify false positives. Document security events daily, creating activity reports for future use and reference. Assist in sales and human resources functions by training staff and contractors."
+	}, {
+		"employer": "MediaLime Co-Owner",
+		"title": "Owner/Web-Designer/Photografer",
+		"location": "London, ON",
+		"dates": "2004-2005",
+		"description": "Company specialized in real estate virtual tours and video tours, providing professional Videos and 360° home virtual tours as well as 360° commercial virtual tours across southwestern Ontario."
+	}, {
+		"employer": "ProAves",
+		"title": "Scientist",
+		"location": "Barbacoas, Colombia",
+		"dates": "2003 - 2004",
+		"description": "Captured mammals, birds, reptiles and amphibians using different type of methods of hand capture, mist nets, pitfall traps and Tomahawk traps. As well as collecting vital biological data such as location, weight, measures and photographical record. Collected and analyzed geographical data about distribution and relationships among organisms’ populations. Successfully finding new distribution reports for wildlife animal species. Providing a basis for future monitoring and an ecological basis for land management and conservation decisions."
+	}]
+};
+
+var projects = {
+	"project" : [
+		{
+			"title" : "Portfolio Site",
+			"date" : "January, 2016",
+			"description" : "A PDF-file was privides and I replicated that design in HTML and CSS. I developed a responsive website that will display images, descriptions and links to each of the portfolio projects I will completing throughout the course of the Front-End Web Developer Nanodegree.",
+			"url" : "https://github.com/ozkarlav/P1-Build-a-Portfolio-Site",
+			"images" : ["images/portfolio01-thumbnail.jpg", "images/portfolio02-thumbnail.jpg"]
+		},{
+			"title" : "About Me",
+			"date" : "January, 2016",
+			"description" : "I created and edited simple HTML and CSS files and created my very first webpage! I was able to complete this project after reviewing Lesson 1 of the Intro to HTML & CSS course.",
+			"url" : "https://github.com/ozkarlav/PO-About-Me",
+			"images" : ["images/about-me-01.jpg", "images/about-me-02.jpg"]
+		},
+		{
+			"title" : "HealthQ",
+			"date" : "2013",
+			"description" : "This was a graduation project for the GIS program from Fanshawe College. Its pourpose was to help users find the nearest Walk-in Clinic, its business hours, contact info and location.",
+			"url" : "http://www.healthq.ca",
+			"images" : ["images/healthq-map-thumbnail.jpg", "images/healthq-team-thumbnail.jpg"]
+		}
+	]
+};
+
+var education = {
+	"schools" : [
+		{
+
+			"name" : "Fanshawe College",
+			"location" : "London, ON",
+			"degree" : "Geographic Information Systems - Graduate Certificate",
+			"major" : "N/A",
+			"dates" : 2014,
+		},
+		{
+			"name" : "Universidad del Tolima",
+			"location" : "Ibague, Colombia",
+			"degree" : "Bachelor of Science",
+			"major" : "Biology",
+			"dates" : 2005,
+		}
+	],
+	"onlinecourses" : [
+	{
+		"school" : "Udacity",
+		"title" : "Front-End Web Developer Nanodegree",
+		"dates" : 2016,
+		"url" : "http://www.udacity.com/"
+	}
+	]
+};
